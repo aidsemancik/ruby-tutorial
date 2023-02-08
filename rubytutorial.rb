7b2901e9cd058dd8c8f1ca9ccc18c5a7a25f211e
[aidsemancik] <[asemancik14@gmail.com]>
2023-02-08 13:05:42 -0600

    learning variables

--- rubytutorial.rb
+++ rubytutorial.rb
@@ -7,9 +7,18 @@
 
     # Puts not only prints the code but "puts" following code on a new line instead of side by side
 
-puts "   /|"
-puts "  / |"
-puts " /  |"
-puts "/___|"
+# puts "   /|"
+# puts "  / |"
+# puts " /  |"
+# puts "/___|"
 
 # Variables
+
+character_name = "John"
+character_age = "35"
+
+puts ("There was once a man named " + character_name)
+puts ("he was " + character_age + " years old." )
+puts ("He really liked the name " + character_name)
+puts ("but didn't like being " + character_age)
+
