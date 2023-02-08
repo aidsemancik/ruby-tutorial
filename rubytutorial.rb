15c8a22e4ceaea4af33286a4c544c85d3a63db8f
[aidsemancik] <[asemancik14@gmail.com]>
2023-02-08 11:51:04 -0600

    learned print and puts

--- rubytutorial.rb
+++ rubytutorial.rb
@@ -1,1 +1,15 @@
-print "Hello World!"
+# Printing onto Screen - Drawing a Shape
+
+# puts "Mike"
+# puts " is cool"
+# print "Hello World!"
+# print " is cool" 
+
+    # Puts not only prints the code but "puts" following code on a new line instead of side by side
+
+puts "   /|"
+puts "  / |"
+puts " /  |"
+puts "/___|"
+
+# Variables
