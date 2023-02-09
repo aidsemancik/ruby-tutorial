23dfae213be8adc6f84b919c010000e0254d7bb8
[aidsemancik] <[asemancik14@gmail.com]>
2023-02-09 11:24:04 -0600

    learned variables

--- rubytutorial.rb
+++ rubytutorial.rb
@@ -14,11 +14,12 @@
 
 # Variables
 
-character_name = "John"
+character_name = "Mike"
 character_age = "35"
 
 puts ("There was once a man named " + character_name)
 puts ("he was " + character_age + " years old." )
+character_name = "Tom"
 puts ("He really liked the name " + character_name)
 puts ("but didn't like being " + character_age)
 
