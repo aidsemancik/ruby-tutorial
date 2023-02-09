d32d101e0fb3d7178bad3dc61967be0414ab0889
[aidsemancik] <[asemancik14@gmail.com]>
2023-02-09 11:31:12 -0600

    learned data types

--- rubytutorial.rb
+++ rubytutorial.rb
@@ -14,12 +14,29 @@
 
 # Variables
 
-character_name = "Mike"
-character_age = "35"
+# character_name = "Mike"
+# character_age = "35"
 
-puts ("There was once a man named " + character_name)
-puts ("he was " + character_age + " years old." )
-character_name = "Tom"
-puts ("He really liked the name " + character_name)
-puts ("but didn't like being " + character_age)
+# puts ("There was once a man named " + character_name)
+# puts ("he was " + character_age + " years old." )
+# character_name = "Tom"
+# puts ("He really liked the name " + character_name)
+# puts ("but didn't like being " + character_age)
 
+# Data Types
+
+    # String
+    name = "Mike"
+
+    # Integer
+    age = 75
+
+    # Float
+    gpa = 3.2
+
+    # Boolean (True/False)
+    ismale = true
+    istall = false
+
+    # nil (no value)
+    flaws = nil
