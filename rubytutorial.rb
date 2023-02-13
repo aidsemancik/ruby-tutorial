b2517eb88b8738fac61ce44a9d689c1fc7b98fd6
[aidsemancik] <[asemancik14@gmail.com]>
2023-02-13 11:40:55 -0600

    learned user input

--- rubytutorial.rb
+++ rubytutorial.rb
@@ -113,3 +113,10 @@
 
 # Getting User Input
 
+    # gets -- user input
+    # .chomp() -- Gets ride of new line after enter
+puts "Enter Your Name: "
+name = gets.chomp()
+puts "Enter Your Age: "
+age = gets.chomp()
+puts ("Hello " + name + ", you are " + age) 
