49f5adfe1f62e86f8815678bb2debf419a387226
[aidsemancik] <[asemancik14@gmail.com]>
2023-02-13 13:05:32 -0600

    learning arrays

--- rubytutorial.rb
+++ rubytutorial.rb
@@ -134,13 +134,20 @@
 
 # Building a Mad Libs Game
 
-puts "Enter a color: "
-color = gets.chomp()
-puts "Enter a plural noun: "
-plural_noun = gets.chomp()
-puts "Enter a celebrity: "
-celebrity = gets.chomp()
+# puts "Enter a color: "
+# color = gets.chomp()
+# puts "Enter a plural noun: "
+# plural_noun = gets.chomp()
+# puts "Enter a celebrity: "
+# celebrity = gets.chomp()
 
-puts ("Roses are " + color)
-puts (plural_noun + " are blue")
-puts ("I love " + celebrity)
+# puts ("Roses are " + color)
+# puts (plural_noun + " are blue")
+# puts ("I love " + celebrity)
+
+# Arrays
+
+friends = Array["Kevin", "Karen", "Oscar"]
+friends[0] = "Dwight"
+friends[]
+puts friends[-2]
