b5e7171056e54eb99c275d1cf7f61e1065966bcd
[aidsemancik] <[asemancik14@gmail.com]>
2023-02-13 11:47:11 -0600

    built a calculator

--- rubytutorial.rb
+++ rubytutorial.rb
@@ -115,8 +115,19 @@
 
     # gets -- user input
     # .chomp() -- Gets ride of new line after enter
-puts "Enter Your Name: "
-name = gets.chomp()
-puts "Enter Your Age: "
-age = gets.chomp()
-puts ("Hello " + name + ", you are " + age) 
+# puts "Enter Your Name: "
+# name = gets.chomp()
+# puts "Enter Your Age: "
+# age = gets.chomp()
+# puts ("Hello " + name + ", you are " + age) 
+
+# Building a Calculator
+
+puts "Enter a number: "
+num1 = gets.chomp().to_f
+
+puts "Enter another number: "
+num2 = gets.chomp().to_f
+
+    # .to_i -- to integer, .to_f -- to float
+puts (num1 + num2)
