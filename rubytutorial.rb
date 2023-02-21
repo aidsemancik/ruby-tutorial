35d828d98ed43bb100fa3f141a75f460ec96008b
[aidsemancik] <[asemancik14@gmail.com]>
2023-02-21 12:49:04 -0600

    learned for loops

--- rubytutorial.rb
+++ rubytutorial.rb
@@ -288,9 +288,29 @@
 
 # While Loops
 
-index = 1
+# index = 1
 
-while index <= 8
-    puts index 
-    index += 1
+# while index <= 8
+#     puts index 
+#     index += 1
+# end
+
+# For Loops
+
+friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+
+for friend in friends
+    puts friend
+end
+
+friends.each do |friend|
+    puts friend
+end
+
+for index in 0..5
+    puts index
+end
+
+6.times do |index|
+    puts index
 end
