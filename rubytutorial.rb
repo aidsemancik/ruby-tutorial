108e987b62c6c20df2137f01bdc05ea93d51b3f3
[aidsemancik] <[asemancik14@gmail.com]>
2023-02-21 12:50:38 -0600

    finished learning ruby basics

--- rubytutorial.rb
+++ rubytutorial.rb
@@ -297,20 +297,22 @@
 
 # For Loops
 
-friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
+# friends = ["Kevin", "Karen", "Oscar", "Angela", "Andy"]
 
-for friend in friends
-    puts friend
-end
+# for friend in friends
+#     puts friend
+# end
 
-friends.each do |friend|
-    puts friend
-end
+# friends.each do |friend|
+#     puts friend
+# end
 
-for index in 0..5
-    puts index
-end
+# for index in 0..5
+#     puts index
+# end
 
-6.times do |index|
-    puts index
-end
+# 6.times do |index|
+#     puts index
+# end
+
+# Finished learning Ruby basics!!!
